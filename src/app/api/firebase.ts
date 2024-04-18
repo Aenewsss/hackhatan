2ae2548a3d41763986263c6c27958be35909bab3
aenewsss <aenamartinelli@gmail.com>
2024-04-18 17:41:51 -0300

    Ok

--- src/app/api/firebase.ts
+++ src/app/api/firebase.ts
@@ -10,7 +10,7 @@ class FirebaseConfig {
     initializeFirebaseApp() {
         const firebaseConfig = {
             apiKey: process.env.NEXT_PUBLIC_API_KEY,
-            authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+            authDomain: process.env.NODE_ENV == "development" ? process.env.NEXT_PUBLIC_AUTH_DOMAIN : "hackathan.vercel.app",
             projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
             storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
             messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
