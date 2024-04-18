e2c546dc3c5e8bdf6f3cddecb4685f534087fb08
aenewsss <aenamartinelli@gmail.com>
2024-04-18 17:18:04 -0300

    novo metodo de signin

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,33 +1,53 @@
 import Firebase from "@/app/api/firebase";
-import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth"
-// @ts-ignore
-import cookie from "js-cookie"
+import { GoogleAuthProvider, signInWithPopup, getAuth, signInWithRedirect, getRedirectResult } from "firebase/auth";
+import cookie from "js-cookie";
 
 //initialize App 
-Firebase.initializeFirebaseApp()
+Firebase.initializeFirebaseApp();
 
-const provider = new GoogleAuthProvider()
-
+const provider = new GoogleAuthProvider();
 const auth = getAuth();
 
 export const login = () => {
-    signInWithPopup(auth, provider)
-        .then(async (result) => {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential?.accessToken;
-            // The signed-in user info.
-            const user = result.user;
-            cookie.set('hackathan_token', await user.getIdToken())
-            window.location.assign('/')
-        }).catch((error) => {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // The email of the user's account used.
-            const email = error.customData.email;
-            // The AuthCredential type that was used.
-            const credential = GoogleAuthProvider.credentialFromError(error);
-            // ...
-        });
+    // Verificar se o usuário está em um dispositivo móvel
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        signInWithRedirect(auth, provider);
+    } else {
+        signInWithPopup(auth, provider)
+            .then(async (result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential?.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                cookie.set('hackathan_token', await user.getIdToken())
+                window.location.assign('/')
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.customData.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                // ...
+            });
+    }
 }
+
+getRedirectResult(auth)
+    .then((result) => {
+        // This gives you a Google Access Token. You can use it to access Google APIs.
+        alert(`chegou aqui ${result}`)
+        console.log('result', result)
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+    }).catch((error) => {
+        console.log('error', error)
+        // ...
+    });
