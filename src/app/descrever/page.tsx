8d7918a25276c61da308a2de830328d52263256b
aenewsss <aenamartinelli@gmail.com>
2024-04-19 13:15:06 -0300

    suspense adicionado

--- src/app/descrever/page.tsx
+++ src/app/descrever/page.tsx
@@ -1,14 +1,17 @@
 import Header from "@/components/Header";
 import DocumetsToDoList from "./DocumentsToDoList";
 import SwitchAction from "./SwitchAction";
+import { Suspense } from "react";
 
 export default function Describe() {
 
     return (
         <main className="px-4 mb-[200px]">
             <Header />
-            <SwitchAction />
-            <DocumetsToDoList />
+            <Suspense fallback={<div className="text-black">Carregando</div>} >
+                <SwitchAction />
+                <DocumetsToDoList />
+            </Suspense>
         </main>
     )
 }
